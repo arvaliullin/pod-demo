ad0220d51873ce789e6608b09d8dd1998a767bf9
Artur Valiullin <ar.valiullin@yandex.ru>
2024-11-14 00:46:19 +0300

    save

--- scripts/pod.ps1
+++ scripts/pod.ps1
@@ -1,3 +1,9 @@
-podman pod create -p 8080:8080 --name mypod --volume ./html:/var/www/html:z
-podman create --pod mypod --name myapp quay.io/rhatdan/myimage
-podman create --pod mypod --name time --workdir /var/www/html ubi8 ./time.sh
+podman pod create -p 8080:80 --name mypod --volume ./html:/usr/share/nginx/html:z
+podman create --pod mypod --name myapp nginx
+podman create --pod mypod --name time --workdir /usr/share/nginx/html ubi8 ./time.sh
+podman pod start mypod
+
+podman pod rm --all --force
+podman rm --all --force
+
+podman play kube mypod.yaml
